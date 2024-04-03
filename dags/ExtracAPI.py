ffe27c8df0daa0f6057909add15c2c14052642c2
luchim11 <luchitrading@gmail.com>
2024-04-03 10:51:27 -0300

    agrego el .gitignore

--- dags/ExtracAPI.py
+++ dags/ExtracAPI.py
@@ -16,16 +16,16 @@ def Extraer_data():
     return datos
 
 datos=Extraer_data()
-df=pd.DataFrame.from_dict(datos['data']).reset_index().rename(columns={"index":'indice'})
+df=pd.DataFrame.from_dict(datos[data]).reset_index().rename(columns={"index":indice})
 
-hora_actual = pd.to_datetime('now')
+hora_actual = pd.to_datetime(now)
 hora_formateada = hora_actual.strftime("%D %H:%M:%S")
 
 #Aplico un filtro de los datos que me interesan y les agrego la hora de Extaccion 
 def Filtrar_data():
-    df_filtrado= df.loc[0:15,['indice','id','symbol','name','rank','price_usd','percent_change_24h','percent_change_1h','market_cap_usd']]
+    df_filtrado= df.loc[0:15,[indice,id,symbol,name,rank,price_usd,percent_change_24h,percent_change_1h,market_cap_usd]]
 
-    hora_actual = pd.to_datetime('now')
+    hora_actual = pd.to_datetime(now)
     hora_formateada = hora_actual.strftime("%D %H:%M:%S")
 
     df_filtrado =df_filtrado.assign(Extraction_time =hora_formateada)
@@ -55,7 +55,7 @@ def conexion_tabla():
             dbname=data_base,
             user=user,
             password=pwd,
-            port='5439'
+            port=5439
         )
         print("Conectado a Postgres")
         
@@ -73,19 +73,19 @@ def cargar_en_postgres():
             dbname=data_base,
             user=user,
             password=pwd,
-            port='5439'
+            port=5439
         )
     dtypes= dataframe.dtypes
     cols= list(dtypes.index )
     tipos= list(dtypes.values)
-    type_map = {'int64': 'INT','int32': 'INT','float64': 'FLOAT','object': 'VARCHAR(50)','bool':'BOOLEAN'}
+    type_map = {int64: INT,int32: INT,float64: FLOAT,object: VARCHAR(50),bool:BOOLEAN}
     sql_dtypes = [type_map[str(dtypes)] for dtypes in tipos]
     # Definir formato SQL VARIABLE TIPO_DATO
     column_defs = [f"{name} {data_type}" for name, data_type in zip(cols, sql_dtypes)]
     # Combine column definitions into the CREATE TABLE statement
     table_schema = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
-            {', '.join(column_defs)}
+            {, .join(column_defs)}
         );
         """
     # Crear la tabla
@@ -94,12 +94,12 @@ def cargar_en_postgres():
     # Generar los valores a insertar
     values = [tuple(x) for x in dataframe.to_numpy()]
     # Definir el INSERT
-    insert_sql = f"INSERT INTO {table_name} ({', '.join(cols)}) VALUES %s"
+    insert_sql = f"INSERT INTO {table_name} ({, .join(cols)}) VALUES %s"
     # Execute the transaction to insert the data
     cur.execute("BEGIN")
     execute_values(cur, insert_sql, values)
     cur.execute("COMMIT")
-    print('Proceso terminado')
+    print(Proceso terminado)
 
 
 #Subida de datos a la base de datos
