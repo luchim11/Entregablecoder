ae12e19db64a7a4a1439f91174672bec7b5a5f38
luchim11 <luchitrading@gmail.com>
2024-04-03 11:11:40 -0300

    .

--- ExtracAPI.py
+++ ExtracAPI.py
@@ -9,65 +9,83 @@ import os
 from psycopg2.extras import execute_values
 
 #Obtengo los datos de la API y los visualizo con PANDAS
-url = "https://api.coinlore.net/api/tickers/?start=0&limit=100"
-response = requests.get(url)
-datos= response.json()
+def Extraer_data():
+    url = "https://api.coinlore.net/api/tickers/?start=0&limit=100"
+    response = requests.get(url)
+    datos=response.json()
+    return datos
 
-df=pd.DataFrame.from_dict(datos[data]).reset_index().rename(columns={"index":indice})
+datos=Extraer_data()
+df=pd.DataFrame.from_dict(datos['data']).reset_index().rename(columns={"index":'indice'})
 
-hora_actual = pd.to_datetime(now)
+hora_actual = pd.to_datetime('now')
 hora_formateada = hora_actual.strftime("%D %H:%M:%S")
 
 #Aplico un filtro de los datos que me interesan y les agrego la hora de Extaccion 
-df_filtrado= df.loc[0:15,[indice,id,symbol,name,rank,price_usd,percent_change_24h,percent_change_1h,market_cap_usd]]
+def Filtrar_data():
+    df_filtrado= df.loc[0:15,['indice','id','symbol','name','rank','price_usd','percent_change_24h','percent_change_1h','market_cap_usd']]
 
-hora_actual = pd.to_datetime(now)
-hora_formateada = hora_actual.strftime("%D %H:%M:%S")
+    hora_actual = pd.to_datetime('now')
+    hora_formateada = hora_actual.strftime("%D %H:%M:%S")
 
-df_filtrado =df_filtrado.assign(Extraction_time =hora_formateada)
+    df_filtrado =df_filtrado.assign(Extraction_time =hora_formateada)
 
-#Cambio los tipos de datos
-df_filtrado["price_usd"]= df_filtrado["price_usd"].astype("float64")
+    #Cambio los tipos de datos
+    df_filtrado["price_usd"]= df_filtrado["price_usd"].astype("float64")
 
-df_filtrado["percent_change_24h"]= df_filtrado["percent_change_24h"].astype("float64")
+    df_filtrado["percent_change_24h"]= df_filtrado["percent_change_24h"].astype("float64")
 
-df_filtrado["percent_change_1h"]= df_filtrado["percent_change_1h"].astype("float64")
+    df_filtrado["percent_change_1h"]= df_filtrado["percent_change_1h"].astype("float64")
 
-df_filtrado["market_cap_usd"]= df_filtrado["market_cap_usd"].astype("float64")
+    df_filtrado["market_cap_usd"]= df_filtrado["market_cap_usd"].astype("float64")
+    return df_filtrado
+
+df_filtrado= Filtrar_data()
 
 #Conexion con Amazon redshift
 url= "data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com"
 data_base= "data-engineer-database"
 user= "luchitrading_coderhouse"
 pwd= "6vJ8t1V7Tp"
-try:
-    conn = psycopg2.connect(
-        host=url,
-        dbname=data_base,
-        user=user,
-        password=pwd,
-        port=5439
-    )
-    print("Conectado a Postgres")
-    
-except Exception as e:
-    print("No es posible conectarse a Postgres")
-    print(e)
+
+def conexion_tabla():
+    try:
+        conn = psycopg2.connect(
+            host=url,
+            dbname=data_base,
+            user=user,
+            password=pwd,
+            port='5439'
+        )
+        print("Conectado a Postgres")
+        
+    except Exception as e:
+        print("No es posible conectarse a Postgres")
+        print(e)
 
 
 #Creacion de tabla en redshift
-def cargar_en_postgres(conn, table_name, dataframe):
+def cargar_en_postgres():
+    dataframe=df_filtrado
+    table_name= "ValoresCryptos"
+    conn = psycopg2.connect(
+            host=url,
+            dbname=data_base,
+            user=user,
+            password=pwd,
+            port='5439'
+        )
     dtypes= dataframe.dtypes
     cols= list(dtypes.index )
     tipos= list(dtypes.values)
-    type_map = {int64: INT,int32: INT,float64: FLOAT,object: VARCHAR(50),bool:BOOLEAN}
+    type_map = {'int64': 'INT','int32': 'INT','float64': 'FLOAT','object': 'VARCHAR(50)','bool':'BOOLEAN'}
     sql_dtypes = [type_map[str(dtypes)] for dtypes in tipos]
     # Definir formato SQL VARIABLE TIPO_DATO
     column_defs = [f"{name} {data_type}" for name, data_type in zip(cols, sql_dtypes)]
     # Combine column definitions into the CREATE TABLE statement
     table_schema = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
-            {, .join(column_defs)}
+            {', '.join(column_defs)}
         );
         """
     # Crear la tabla
@@ -76,14 +94,17 @@ def cargar_en_postgres(conn, table_name, dataframe):
     # Generar los valores a insertar
     values = [tuple(x) for x in dataframe.to_numpy()]
     # Definir el INSERT
-    insert_sql = f"INSERT INTO {table_name} ({, .join(cols)}) VALUES %s"
+    insert_sql = f"INSERT INTO {table_name} ({', '.join(cols)}) VALUES %s"
     # Execute the transaction to insert the data
     cur.execute("BEGIN")
     execute_values(cur, insert_sql, values)
     cur.execute("COMMIT")
-    print(Proceso terminado)
+    print('Proceso terminado')
 
 
 #Subida de datos a la base de datos
-cargar_en_postgres(conn=conn, table_name= "ValoresCryptos", dataframe=df_filtrado)
+Extraer_data()
+Filtrar_data()
+conexion_tabla()
+cargar_en_postgres()
 
--- dags/dagFinal.py
+++ dags/dagFinal.py
@@ -15,42 +15,42 @@ from psycopg2.extras import execute_values
 from  ExtracAPI import Extraer_data, Filtrar_data, conexion_tabla, cargar_en_postgres 
 dag_path = os.getcwd()
 default_args = {
-    start_date: datetime(2024, 4, 2),
-    retries: 1,
-    retry_delay: timedelta(minutes=5)
+    'start_date': datetime(2024, 4, 2),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
 }
 
 #nombre del dag
-ingestion_dag = DAG(
-    dag_id=ingestion_data,
+DagEntrega = DAG(
+    dag_id='DagEntrega',
     default_args=default_args,
-    description=Agregar datos de las top 15 cryptomonedas en una tabla de redshift,
+    description='Agregar datos de las top 15 cryptomonedas en una tabla de redshift',
      schedule_interval=timedelta(days=1),
     catchup=False
 )
 
 
 task_1 = PythonOperator(
-    task_id=Extraerdata,
+    task_id='Extraerdata',
     python_callable=Extraer_data,
     #op_args=["{{ ds }} {{ execution_date.hour }}"],
     dag=ingestion_dag,
 )
 
 task_2 = PythonOperator(
-    task_id=Filtrardatos,
+    task_id='Filtrardatos',
     python_callable=Filtrar_data,
     dag=ingestion_dag,
 )
 
 task_3 = PythonOperator(
-    task_id=conexionconredshift,
+    task_id='conexionconredshift',
     python_callable=conexion_tabla,
     dag=ingestion_dag,
 )
 
 task_4 = PythonOperator(
-    task_id=CargaenDB,
+    task_id='CargaenDB',
     python_callable=cargar_en_postgres,
     dag=ingestion_dag,
 )
